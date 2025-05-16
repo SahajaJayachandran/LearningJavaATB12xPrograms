760695b24712ce6be08889d743483d3c8f1846ce
Sahaja Jayachandran <sahaja932@gmail.com>
2025-05-16 23:14:44 +0530

    11th May

---
+++ src/ex_01_JavaBasics/Lab013_Println_vs_Print.java
@@ -0,0 +1,12 @@
+package ex_01_JavaBasics;
+
+public class Lab013_Println_vs_Print {
+    public static void main(String[] args) {
+        System.out.println("Hi ln"); // newline
+        System.out.println("Bye ln");
+
+        System.out.print("Hi"); // newline
+        System.out.print("Bye");
+
+    }
+}
