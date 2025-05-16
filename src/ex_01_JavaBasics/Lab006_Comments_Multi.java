af78ef9b64b6da5cbdd2452b45c52f35d62c31a5
Sahaja Jayachandran <sahaja932@gmail.com>
2025-05-16 22:51:22 +0530

    11th May

---
+++ src/ex_01_JavaBasics/Lab006_Comments_Multi.java
@@ -0,0 +1,14 @@
+package ex_01_JavaBasics;
+
+public class Lab006_Comments_Multi {
+    public static void main(String[] args) {
+        /**
+
+         * Course :  ATB12x
+         * Learning :  Java
+         *
+         **/
+        System.out.println("Multi Comment");
+    }
+}
+
---
+++ src/ex_01_JavaBasics/Lab008_Comments.java
@@ -0,0 +1,13 @@
+package ex_01_JavaBasics;
+
+public class Lab008_Comments {
+    public static void main(String[] args) {
+
+            // Method implementation
+
+//        System.out.println("1");
+//        System.out.println("2");
+            System.out.println();
+        }
+    }
+
