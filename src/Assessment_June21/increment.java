b2de5372d274f9cb9f2da36f77edd8bf603191f5
Sahaja Jayachandran <sahaja932@gmail.com>
2025-06-21 12:19:00 +0530

    21June_Assessment_CodeChallenge4

---
+++ src/Assessment_June21/increment.java
@@ -0,0 +1,20 @@
+package Assessment_June21;
+
+public class increment {
+    public static void main(String[] args) {
+
+
+        int number = 5;
+
+
+        System.out.println("Pre-increment:" + number);
+
+        System.out.println("Post-increment:" + (number++));  // Use first, then incrementSystem.out.println("After: " + number);
+
+                System.out.println("Pre-decrement: " + number);
+
+
+
+        System.out.println("Post-decrement:" +(number--));
+    }
+}
