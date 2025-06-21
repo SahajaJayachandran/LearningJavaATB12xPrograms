eeb05eb45bcdea0e279c2cfb7fa6f1f69ba7cbcd
Sahaja Jayachandran <sahaja932@gmail.com>
2025-06-21 11:51:21 +0530

    21June_Assessment_CodeChallenge1

---
+++ src/Assessment_June21/VarDeclarationInitialization.java
@@ -0,0 +1,15 @@
+package Assessment_June21;
+
+public class VarDeclarationInitialization {
+    public static void main(String[] args) {
+        int a=20;
+        double b=30.5;
+        boolean c=true;
+        char d='d';
+        System.out.println("a:"+a);
+        System.out.println("b:"+b);
+        System.out.println("c:"+c);
+        System.out.println("d:"+d);
+
+    }
+}
