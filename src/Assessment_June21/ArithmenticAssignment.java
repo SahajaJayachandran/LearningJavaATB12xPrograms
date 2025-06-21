5b17542ede91c82793daa82b7cdafcffdde22a36
Sahaja Jayachandran <sahaja932@gmail.com>
2025-06-21 11:58:33 +0530

    21June_Assessment_CodeChallenge2

---
+++ src/Assessment_June21/ArithmenticAssignment.java
@@ -0,0 +1,42 @@
+package Assessment_June21;
+
+public class ArithmenticAssignment {
+    public static void main(String[] args) {
+
+
+        int num1 = 20;
+        int num2 = 5;
+        int result;
+
+
+        result = num1 + num2;
+        System.out.println("Addition (num1 + num2): " + result);
+
+        result = num1 - num2;
+        System.out.println("Subtraction (num1 - num2): " + result);
+
+        result = num1 * num2;
+        System.out.println("Multiplication (num1 * num2): " + result);
+
+        result = num1 / num2;
+        System.out.println("Division (num1 / num2): " + result);
+
+                int value=1;
+
+
+
+
+        value += 5;
+        System.out.println("After value += 5: " + value);
+
+        value -= 3;
+        System.out.println("After value -= 3: " + value);
+
+        value *= 2;
+        System.out.println("After value *= 2: " + value);
+
+        value /= 4;
+        System.out.println("After value /= 4: " + value);
+
+    }
+}
